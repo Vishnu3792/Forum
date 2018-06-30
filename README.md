3c05e59b70586234685664cbefeb7a845c64c0b2
Vishnu3792 <39777893+Vishnu3792@users.noreply.github.com>
2018-06-30 12:55:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Forum
+"# forum" 
+"# forum" 
---
+++ storage/framework/views/98912f84c4a0fec6a181c54e97b68ac24ba4a09e.php
@@ -0,0 +1,10 @@
+<?php $__env->startSection('content'); ?>
+<main role="main" class="inner cover">
+        <h1 class="cover-heading">Forum</h1>
+        <p class="lead">This Forum is created to stay connected</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
